ee543ab1c7cd3906de538ec9b17ca33e4faaa317
Sean Snow <sean.snow@live.com>
2018-07-20 06:42:12 +0000

    start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+if [ 0"$APP_NAME" = "0" ];
+then
+export APP_NAME=app
+fi
+
+export LOG_FILE=$LOG_PATH/$APP_NAME.log
+
+if [ 0"$DEFAULT_RUN_ARGS" = "0" ];
+then
+export DEFAULT_RUN_ARGS="--logging.file=$LOG_FILE --server.port=1818"
+fi
+
+
+echo PATH=$PATH
+echo DEFAULT_RUN_ARGS=$DEFAULT_RUN_ARGS
+echo RUN_ARGS=$RUN_ARGS
+echo APP_NAME=$APP_NAME
+echo NEW RUN_ARGS = $DEFAULT_RUN_ARGS $RUN_ARGS
+
+$APP_NAME $DEFAULT_RUN_ARGS $RUN_ARGS
